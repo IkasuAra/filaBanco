aac87a1c1be56fc2ececa916a6ec3cd83797cb58
CarolineMano <carol.marinho.mano@gmail.com>
2021-05-10 18:51:48 -0300

    Update Main.java
    Alteração no Main para mostrar Menu

--- Main.java
+++ Main.java
@@ -4,38 +4,55 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("*****Banco Dinheiro na Mão*****");
         ManageAttendance manager = new ManageAttendance(2);
-        boolean endOfProgram = true;
-        Client newClient;
         Scanner input = new Scanner(System.in);
+        boolean endOfProgram = true;
+        int option = 0, age;
+        Client newClient;
         String name;
-        int age, numNewClients;
+        Client auxiliar;
+
         do {
-            System.out.println("\n\nQuantos clientes chegaram? ");
-            numNewClients = input.nextInt();
-            for(int i = 0; i < numNewClients; i++) {
-                System.out.println("Insira seu nome: ");
-                name = input.next();
-                System.out.println("Insira sua idade: ");
-                age = input.nextInt();
-                
-                newClient = new Client(name, age);
-                manager.addClient(newClient);
-            }
+            System.out.println("\nEscolha uma das opções abaixo: ");
+            System.out.println("1 - Chegada do cliente à agência");
+            System.out.println("2 - Verificar quem é o próximo a ser atendido");
+            System.out.println("3 - Atender um cliente");
+            System.out.println("4 - Exibir as filas (idoso e não idoso)");
+            System.out.println("5 - Finalizar o programa (só poderá ser finalizado caso não tenha mais clientes aguardando");
+            System.out.print("Opção: ");
+            
+            option = input.nextInt();
 
-            System.out.println(manager.showQueues());
-            manager.decideNextClient();
-            System.out.println("\nPróximo cliente a ser atendido: ");
-            System.out.println("Nome: " + manager.showNext().getName() + "/Idade:" + manager.showNext().getAge());
-            manager.getNext();  
-
-            System.out.println(manager.showQueues());
-
-            if(manager.numClients() == 0) {
-                endOfProgram = false;
+            
+            switch (option) {
+                case 1:
+                    System.out.print("Insira o nome: ");
+                    name = input.next();
+                    System.out.print("Insira a idade: ");
+                    age = input.nextInt();                            
+                    newClient = new Client(name, age);
+                    manager.addClient(newClient);
+                    break;
+                case 2:
+                    System.out.println("\nPróximo cliente a ser atendido: ");
+                    System.out.println("Nome: " + manager.showNext().getName() + "/Idade:" + manager.showNext().getAge());
+                    break;
+                case 3:
+                    auxiliar = manager.getNext(); 
+                    System.out.println("\nCliente atendido: ");
+                    System.out.println("Nome: " + auxiliar.getName() + "/Idade:" + auxiliar.getAge());
+                    break;
+                case 4:
+                    System.out.println(manager.showQueues());   
+                    break;
+                case 5:
+                    endOfProgram = !(manager.numClients() == 0);
+                    break;
+                default:
+                    System.out.println("Selecione uma opção válida!");
+                    break;
             }
 
         } while (endOfProgram);
-
         input.close();
     }
 }
