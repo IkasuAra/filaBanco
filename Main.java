fb77e817a213834dc6f94edf537643bb533ae588
CarolineMano <carol.marinho.mano@gmail.com>
2021-05-10 15:17:13 -0300

    Update Main.java
    Primeira versão da classe Main, com os primeiros testes das funcionalidades das demais classes

--- Main.java
+++ Main.java
@@ -1,20 +1,41 @@
 import java.util.Scanner;
 
-/*
-1 – Chegada do cliente na agência
-2 – Verificar quem é o próximo a ser atendido
-3 – Atender um cliente (ao atender o cliente, o nome e a idade dele deve ser apresentada)
-4 – Exibir as filas (idoso e não idoso)
-5 – Finalizar o programa (que só poderá ser finalizado caso não tenha mais clientes aguardando)
-
-O programa somente pode encerrar com a opção 5 do menu. Para qualquer valor digitado que não
-esteja no menu deve aparecer uma mensagem informando que aquela opção é inválida.
-*/
-
 public class Main {
     public static void main(String[] args) {
-        Scanner entrada = new Scanner(System.in);
-        
-        entrada.close();
+        System.out.println("*****Banco Dinheiro na Mão*****");
+        ManageAttendance manager = new ManageAttendance(2);
+        boolean endOfProgram = true;
+        Client newClient;
+        Scanner input = new Scanner(System.in);
+        String name;
+        int age, numNewClients;
+        do {
+            System.out.println("\n\nQuantos clientes chegaram? ");
+            numNewClients = input.nextInt();
+            for(int i = 0; i < numNewClients; i++) {
+                System.out.println("Insira seu nome: ");
+                name = input.next();
+                System.out.println("Insira sua idade: ");
+                age = input.nextInt();
+                
+                newClient = new Client(name, age);
+                manager.addClient(newClient);
+            }
+
+            System.out.println(manager.showQueues());
+            manager.decideNextClient();
+            System.out.println("\nPróximo cliente a ser atendido: ");
+            System.out.println("Nome: " + manager.showNext().getName() + "/Idade:" + manager.showNext().getAge());
+            manager.getNext();  
+
+            System.out.println(manager.showQueues());
+
+            if(manager.numClients() == 0) {
+                endOfProgram = false;
+            }
+
+        } while (endOfProgram);
+
+        input.close();
     }
 }
